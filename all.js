5dae9a39532b9045d40c2683d4c2c776fda9c891
Midomommy <tinasue165@gmail.com>
2023-10-15 19:24:15 +0800

    setup file

---
+++ all.js
@@ -0,0 +1,41 @@
+const { createApp, ref, component } = Vue;
+const app = Vue.createApp({
+  components: {
+    "header-component": {
+      template: "#header-component",
+    },
+  },
+  setup() {
+    const views = ref({});
+
+    //   onMounted(()=>{
+
+    // })
+
+    getViewsData();
+    function getViewsData() {
+      axios.get("http://localhost:3000/views").then((res) => {
+        views.value = res.data;
+        console.log(res.data);
+      });
+    }
+
+    return {
+      views,
+    };
+  },
+});
+// const app = Vue.createApp({});
+app.component("header-component", {
+  template: "#header-component", // 這裡是元件的模板
+});
+app.mount("#app");
+
+// axios
+//   .post("http://localhost:3000/posts", {
+//     title: "json-server",
+//     author: "typicode2",
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
---
+++ componets/header-component.js
@@ -0,0 +1,8 @@
+const { createApp, ref } = Vue;
+Vue.createApp({
+  setup() {
+    return {
+      data: ref(0),
+    };
+  },
+}).mount("#header-component");
